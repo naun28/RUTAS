64997e55c41433670ae65e4790df4cc977c90717
AlbertoR17 <alberto9406@gmail.com>
2018-03-05 12:23:54 -0700

    borre b
    b borrada

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Sistema de Rutas bbbbbbbbbbbbbb
+                    Sistema de Rutas
                 </div>
 
                 <div class="links">
