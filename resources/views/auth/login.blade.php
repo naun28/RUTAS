db91b33833195f8b6ee99fab7d15343baab9d2a3
AlbertoR17 <alberto9406@gmail.com>
2018-03-05 12:30:18 -0700

    cambios en login
    cambios en login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,8 +4,11 @@
 <div class="middle-box text-center loginscreen animated fadeInDown">
     <div>
       <div><h1 class="logo-name">SEC</h1></div>
-      <h3>Aula de Medios</h3>
-     
+      <h3>Aula de medios</h3>
+      <!--<p>Perfectly designed and precisely prepared admin theme with over 50 pages with extra new web app views.
+          Continually expanded and constantly improved Inspinia Admin Them (IN+)
+      </p>
+      <p>Login in. To see it in action.</p>-->
       <form class="m-t" role="form" method="POST" action="{{ route('login') }}">
        {{ csrf_field() }}
        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
@@ -24,12 +27,14 @@
             </span>
         @endif
        </div>
-       <button type="submit" class="btn btn-danger block full-width m-b">Entrar</button>
+       <button type="submit" class="btn btn-primary block full-width m-b">Entrar</button>
 
+       {{-- <a href="{{ route('password.request') }}"><small>Forgot password?</small></a> 
        <p class="text-muted text-center"><small>No tienes una cuenta?</small></p>
-       <a class="btn btn-sm btn-white btn-block" href="{{ route('register') }}">Crear una cuenta</a>
+       <a class="btn btn-sm btn-white btn-block" href="{{ route('register') }}">Crea una cuenta</a>--}}
       </form>
-      <p class="m-t"> <small>Secretaria de Educacion y Cultura &copy; {{ date('Y') }}</small> </p>
+      <p class="m-t"> <small>Secretaria de Educación y Cultura &copy; {{ date('Y') }}</small> </p>
     </div>
 </div>
 @endsection
+
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,12 +4,11 @@
 <div class="middle-box text-center loginscreen   animated fadeInDown">
     <div>
         <div> <h1 class="logo-name">SEC</h1></div>
-        <h3>Registrar Usuario</h3>
-        
+        <h3>Registrar usuario</h3>
         <form class="m-t" role="form"  method="POST" action="{{ route('register') }}">
           {{ csrf_field() }}
           <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-            <input type="text" class="form-control" placeholder="Nombre" name="name" value="{{ old('name') }}" required autofocus>
+            <input type="text" class="form-control" placeholder="Name" name="name" value="{{ old('name') }}" required autofocus>
             @if ($errors->has('name'))
                 <span class="help-block">
                     <strong>{{ $errors->first('name') }}</strong>
@@ -17,7 +16,7 @@
             @endif
           </div>
           <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-            <input type="email" class="form-control" placeholder="Correo" name="email" value="{{ old('email') }}" required>
+            <input type="email" class="form-control" placeholder="Email" name="email" value="{{ old('email') }}" required>
             @if ($errors->has('email'))
                 <span class="help-block">
                     <strong>{{ $errors->first('email') }}</strong>
@@ -25,7 +24,7 @@
             @endif
           </div>
           <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-            <input type="password" class="form-control" placeholder="Contraseña" name="password" required>
+            <input type="password" class="form-control" placeholder="Password" name="password" required>
             @if ($errors->has('password'))
                 <span class="help-block">
                     <strong>{{ $errors->first('password') }}</strong>
@@ -33,17 +32,17 @@
             @endif
           </div>
           <div class="form-group">
-            <input type="password" class="form-control" placeholder="Confirmar Contraseña" name="password_confirmation" required>
+            <input type="password" class="form-control" placeholder="Confirm Password" name="password_confirmation" required>
           </div>
           <div class="form-group">
-            <div class="checkbox i-checks"><label> <input type="checkbox"><i></i> Recordar Cuenta </label></div>
+            <div class="checkbox i-checks"><label> <input type="checkbox"><i></i> Acepta los terminos y condiciones </label></div>
           </div>
           <button type="submit" class="btn btn-primary block full-width m-b">Registrar</button>
 
-          <p class="text-muted text-center"><small>Ya tienes cuenta?</small></p>
+          <p class="text-muted text-center"><small>Ya tienes una cuenta</small></p>
           <a class="btn btn-sm btn-white btn-block" href="{{ route('login') }}">Entrar</a>
         </form>
-        <p class="m-t"> <small>Secretaria de Educacion y Cultura &copy; {{ date('Y') }}</small> </p>
+        <p class="m-t"> <small>Secretaria de Educación y Cultura &copy; {{ date('Y') }}</small> </p>
     </div>
 </div>
 @endsection
