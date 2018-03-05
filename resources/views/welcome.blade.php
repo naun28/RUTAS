4b358784ba9d149b6ba5e6327fe05d9a53428c74
naun28 <naaunlara@gmail.com>
2018-03-05 10:56:42 -0700

    primer commit
    este es el primer commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Rutas Inspinia
+                    Sistema de Rutas 
                 </div>
 
                 <div class="links">
