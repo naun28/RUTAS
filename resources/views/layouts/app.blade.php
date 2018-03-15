bc482e9b518a7736cd17a2e22a5fa04b87fd8cc2
naun28 <naaunlara@gmail.com>
2018-03-15 12:21:36 -0700

    iconos
    le cambie los iconos

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,17 +18,17 @@
     <li class="active">
       <a href="{{ route('home') }}"><i class="fa fa-home"></i> <span class="nav-label">Inicio</span></a>
     </li>
-     <li class="active">
-      <a href="{{ route('escuelas') }}"><i class="fa fa-user-circle-o"></i> <span class="nav-label">Lista de Escuelas</span></a>
-    </li>
     <li class="active">
       <a href="{{ route('usuarios') }}"><i class="fa fa-user-circle-o"></i> <span class="nav-label">Agregar Usuarios</span></a>
     </li>
     <li class="active">
-      <a href="{{ route('agendar') }}"><i class="fa fa-address-book"></i> <span class="nav-label">Agendar Rutas</span></a>
+      <a href="{{ route('perfilesusuarios') }}"><i class="fa fa-vcard-o"></i> <span class="nav-label">Perfil de Usuarios</span></a>
+    </li>
+     <li class="active">
+      <a href="{{ route('escuelas') }}"><i class="fa fa-list-alt"></i> <span class="nav-label">Lista de Escuelas</span></a>
     </li>
     <li class="active">
-      <a href="{{ route('perfilesusuarios') }}"><i class="fa fa-home"></i> <span class="nav-label">Perfil de Usuarios</span></a>
+      <a href="{{ route('agendar') }}"><i class="fa fa-address-book"></i> <span class="nav-label">Agendar Rutas</span></a>
     </li>
     <li class="active">
       <a href="{{ route('costo') }}"><i class="fa fa-usd"></i> <span class="nav-label">Viáticos</span></a>
@@ -37,10 +37,10 @@
       <a href="{{ route('listado') }}"><i class="fa fa-list"></i> <span class="nav-label">Listado</span></a>
     </li> --}}
     <li class="active">
-      <a href="{{ route('brigadista') }}"><i class="fa fa-user-o"></i> <span class="nav-label">Brigadista</span></a>
+      <a href="{{ route('brigadista') }}"><i class="fa fa-male"></i> <span class="nav-label">Brigadista</span></a>
     </li>        
     <li class="active">
-      <a href="{{ route('secretaria') }}"><i class="fa fa-user-o"></i> <span class="nav-label">Secretarias</span></a>
+      <a href="{{ route('secretaria') }}"><i class="fa fa-female"></i> <span class="nav-label">Secretarias</span></a>
     </li>
     <li class="active">
       <a href="{{ route('reporte') }}"><i class="fa fa-newspaper-o"></i> <span class="nav-label">Reportes</span></a>
